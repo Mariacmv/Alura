0d304e314c10ff42dd00f24d782fa963aa18f410
Maria Clara <mariaclaragmviana@gmail.com>
2025-07-25 11:00:20 -0300

    Terminando o módulo 3

---
+++ Java/projeto1/Condicional.java
@@ -0,0 +1,20 @@
+public class Condicional{
+  public static void main(String[] args){
+    int anoDeLancamento = 2022;
+    boolen incluidoNoPlano = true;
+    double notaDoFIlme = 8.1;
+    String tipoPlano = "plus";
+    
+    if (anoDeLancamento >= 2022){
+      System.out.println("Lançamento que os clientes estão curtindo!");
+    } else{
+      System.out.println("Filme retrô que vale a pena assistir!");
+    }
+
+    if (incluidoNoPlano || tipoPlano.equals("plus")){ //ou if (incluidoNoPlano == true) mas o ideal é com equals()
+      System.out.println("Filme liberado");
+    } else{
+      System.out.println("Deve pagar a locação");
+    }
+  }
+}
