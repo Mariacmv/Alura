56afd415803856005d6ee24278f2c19bcb6b90dc
Mariacmv <mariaclaragmviana@gmail.com>
2025-07-31 10:13:12 -0300

    Terminando os desafios

---
+++ Java/projeto1/desafios/Fatorial.java
@@ -0,0 +1,16 @@
+package desafios;
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        for(int aux = 1; aux <= numero; aux++){
+            
+        }
+
+        scanner.close();
+    }
+}
